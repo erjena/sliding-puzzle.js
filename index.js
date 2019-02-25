7725039b82514a681be400350a6477a2ca18a07c
Erzhena Shirabon <erzhena.shirabon@gmail.com>
2019-02-24 20:04:33 -0800

    adder bound check

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const TOTAL_MOVES = 15;
 
-function state(table, x, y) {
-    this.table = table;
-    this.x = x;
-    this.y = y;
-    this.moveCount = 0;
-}
-
 function generateState() {
     var table = [
         [1, 2, 3, 4],
@@ -14,7 +7,15 @@ function generateState() {
         [9, 10, 11, 12],
         [13, 14, 15, 'x']
     ];
-    return new state(table, 3, 3);
+
+    return {
+        table: table,
+        x: 3,
+        y: 3,
+        moveCount: 0,
+        isActive: true,
+        isWon: false
+    };
 }
 
 function prettyPrint(state) {
@@ -25,44 +26,54 @@ function prettyPrint(state) {
             playBoard.rows[i].cells[j].innerHTML = table[i][j];
         }
     }
-    var moveCount = document.getElementById('moveCount');
-    moveCount.innerHTML = state.moveCount;
+    var statusBoard = document.getElementById('statusBoard');
+    if (state.isActive) {
+        statusBoard.innerHTML = "Your number of moves: " + state.moveCount;
+    } else if (state.isWon) {
+        statusBoard.innerHTML = "You won!";
+    } else {
+        statusBoard.innerHTML = "Game Over! Try Again!";
+    }
 }
 
 function moveUp(state) {
     if (state.x === 0) {
-        return;
+        return false;
     }
     state.table[state.x][state.y] = state.table[state.x-1][state.y];
     state.table[state.x-1][state.y] = 'x';
     state.x = state.x-1;
+    return true;
 }
 
 function moveDown(state) {
     if (state.x === 3) {
-        return;
+        return false;
     }
     state.table[state.x][state.y] = state.table[state.x+1][state.y];
     state.table[state.x+1][state.y] = 'x';
     state.x = state.x+1;
+    return true;
 }
 
 function moveLeft(state) {
     if (state.y === 0) {
-        return;
+        return false;
     }
     state.table[state.x][state.y] = state.table[state.x][state.y-1];
     state.table[state.x][state.y-1] = 'x';
     state.y = state.y-1;
+    return true;
 }
 
 function moveRight(state) {
     if (state.y === 3) {
-        return;
+        return false;
     }
     state.table[state.x][state.y] = state.table[state.x][state.y+1];
     state.table[state.x][state.y+1] = 'x';
     state.y = state.y+1;
+    return true;
 }
 
 function randomShift() {
@@ -71,62 +82,56 @@ function randomShift() {
     return arrOfFunctions[randomIndex];
 }
 
-// function processInput(state) {
-//     rl.question(`Left ${TOTAL_MOVES-state.moveCount}. Your move: `, function(answer) {
-//         if (answer == 'u') {
-//             moveUp(state);
-//             state.moveCount++;
-//         } else if (answer == 'd') {
-//             moveDown(state);
-//             state.moveCount++;
-//         } else if (answer == 'l') {
-//             moveLeft(state);
-//             state.moveCount++;
-//         } else if (answer == 'r') {
-//             moveRight(state);
-//             state.moveCount++;
-//         } else {
-//             console.log(`Enter 'u', 'd', 'l' or 'r'`);
-//         }
-//         prettyPrint(state);
-
-//         if (state.moveCount >= TOTAL_MOVES) {
-//             console.log(`Game Over! Try Again!`)
-//             rl.close();
-//             return;
-//         }
-//         processInput(state);
-//     });
-// }
-
 var state = generateState();
 
 function handleKeyPress(e) {
     if (e == undefined) {
         e = window.event;
     }
+    if (state.isActive === false) {
+        return;
+    }
+
     if (e.keyCode === 97) {
-        moveLeft(state);
-        state.moveCount++;
+        if (moveLeft(state) === true) {
+            state.moveCount++;
+        }
     } else if (e.keyCode === 100) {
-        moveRight(state);
-        state.moveCount++;
+        if (moveRight(state) === true) {
+            state.moveCount++;
+        }    
     } else if (e.keyCode === 119) {
-        moveUp(state);
-        state.moveCount++;
+        if (moveUp(state) === true) {
+            state.moveCount++;
+        }
     } else if (e.keyCode === 115) {
-        moveDown(state);
-        state.moveCount++;
+        if (moveDown(state) === true) {
+            state.moveCount++;
+        }
+    }
+
+    if (isWinner()) {
+        state.isWon = true;
+        state.isActive = false;
+    } else if (state.moveCount >= TOTAL_MOVES) {
+        state.isActive = false;
     }
     prettyPrint(state);
 }
 
-function handleBodyLoad() {
+function isWinner() {
+    var table = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 'x']
+    ];
+    return JSON.stringify(table) == JSON.stringify(state.table);
+}
 
-    for (var i = 0; i < 15; i++) {
+function startGame() {
+    for (var i = 0; i < 1; i++) {
         randomShift()(state);
     }
     prettyPrint(state);
-
-    //processInput(state);
 }
